5154a060f9e772f65e8f4b640347db5c7999bd16
Vamproical <mimoun136@yandex.ru>
2020-07-05 11:33:13 +0900

    Complete stage 1.

--- Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/task/src/sorting/Main.java
@@ -5,10 +5,20 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int countNumbers = 0;
+        int countMaxNumber = 0;
+        long maxNumber = 0L;
         while (scanner.hasNextLong()) {
             long number = scanner.nextLong();
-            // write your code here
+            ++countNumbers;
+            if (maxNumber < number) {
+                maxNumber = number;
+                countMaxNumber = 1;
+            } else if (number == maxNumber) {
+                countMaxNumber++;
+            }
         }
+        System.out.println("Total numbers: " + countNumbers + ".");
+        System.out.println("The greatest number: " + maxNumber + " (" + countMaxNumber + " time(s)).");
     }
 }
